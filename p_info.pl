90eed2b5f72b45227ae10222850e267787480297
Corran Musk <corran.musk@googlemail.com>
2012-03-31 12:56:14 +0100

    Added p_info.pl, demonstrates some process indcators in Perl

---
+++ p_info.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+print "Process ID    :\t$$\n";
+print "Real UID      :\t$<\n";
+print "Effective UID :\t$>\n";
+print "Script name   :\t$0\n";
+print "Perl Version  :\t$]\n";
+print "Op sys name   :\t$^O\n";
+
